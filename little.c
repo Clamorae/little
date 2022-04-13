a8621b314f8e53fb7e87e0b1259da6ebf1d4b6c9
Quentin Trombini <trombiniquentin@gmail.com>
2022-04-13 14:26:34 +0200

    getmin is working

--- little.c
+++ little.c
@@ -194,19 +194,19 @@ void build_solution()
     return;
 }
 
-int* getMin(int licol, int isCol, double matrix[NBR_TOWNS][NBR_TOWNS]){
+int* getMin(int licol, int isCol, double matrix[NBR_TOWNS][NBR_TOWNS],int countZero){
     int local_min=99999;
     int place;
     if (isCol){
         for (int i = 0; i < NBR_TOWNS; i++){
-            if(matrix[i][licol]!=-1 && matrix[i][licol]<local_min){
+            if(matrix[i][licol]!=-1 && matrix[i][licol]<local_min && matrix[i][licol]!=countZero){
                 local_min=matrix[i][licol];
                 place = i;
             }
         }
     }else{
-        for (int i = 0; i < sizeof(matrix[0]); i++){
-            if(matrix[i][licol]!=-1 && matrix[licol][i]<local_min){
+        for (int i = 0; i < NBR_TOWNS; i++){
+            if(matrix[licol][i]!=-1 && matrix[licol][i]<local_min && matrix[licol][i]!=countZero){
                 local_min=matrix[licol][i];
                 place = i;
             }
@@ -238,13 +238,13 @@ void little_algorithm(double d0[NBR_TOWNS][NBR_TOWNS], int iteration, double eva
 
     double eval_node_child = eval_node_parent;
     for ( i = 0; i < NBR_TOWNS; i++){
-        int* result = getMin(i,1,d);
+        int* result = getMin(i,0,d,-1);
         d[i][result[1]]=0;
         eval_node_child+=result[0];
     }
 
     for ( i = 0; i < NBR_TOWNS; i++){
-        int* result = getMin(i,1,d);
+        int* result = getMin(i,1,d,-1);
         d[result[1]][i]=0;
         eval_node_child+=result[0];
     }
